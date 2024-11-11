500818a58c0d31a2bafda98dfb600f0fca3ef199
hshaharia064 <hshaharia064@gmail.com>
2024-11-11 11:49:23 +0530

    added contacts and changed navbar
    added contacts section and changed the navbar because previous navbar was not that good also added sliding transition in the hamburger div

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('hidden');
-  }
+  const navLinks = document.querySelector('.nav-links');
+ 
+  navLinks.classList.toggle('-translate-y-96');
+  navLinks.classList.toggle('opacity-0');
 
-  
+  // Call rotateICON to rotate the icon
+  rotateICON();
+}
+
+function rotateICON() {
+  const div = document.getElementById('rotateicon');
+  div.classList.toggle('rotate-90');
+}
